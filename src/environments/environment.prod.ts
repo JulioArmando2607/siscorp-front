b5ff7211c8b76d0e94a2236125b01615fae63dbd
Julio Armando <armandojulio82@gmail.com>
2025-05-19 10:39:41 -0500

    csmbios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiurl: 'http://localhost:8078',
     //apiurl: 'https://ambientepruebas.pais.gob.pe:8078',
     //backendJavaUags:'http://localhost:8076',
-    urlFile: 'https://www.pais.gob.pe/repo-pais/public'
+    //urlFile: 'https://www.pais.gob.pe/repo-pais/public'
 };
